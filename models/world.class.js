a24574859070edf38db668e29aba719c84532e28
Enes Geldi <geldi.dev@gmail.com>
2024-12-15 00:57:29 +0100

    draw backGround img

--- models/world.class.js
+++ models/world.class.js
@@ -1,13 +1,22 @@
 class World {
     ctx;
     canvas;
+    clouds = [
+        new Cloud(),
+    ];
     character = new Character();
     enemies = [
         new Chicken(),
         new Chicken(),
         new Chicken(),
     ];
-    
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 260),
+    ];
+
+
+
+
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
@@ -17,16 +26,26 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height)
-        });
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.backgroundObjects);
 
         // draw wird immer wierder aufgerufen!
         let self = this;
         requestAnimationFrame(function() {
             self.draw();
         })
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        })
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
     }
 
 
