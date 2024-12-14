e1a35af87470be465c2f9d1f03f5942f613a4821
Enes Geldi <geldi.dev@gmail.com>
2024-12-14 23:16:31 +0100

    draw & redraw function on canvas2

--- models/world.class.js
+++ models/world.class.js
@@ -1,5 +1,6 @@
 class World {
     ctx;
+    canvas;
     character = new Character();
     enemies = [
         new Chicken(),
@@ -9,10 +10,13 @@ class World {
     
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
         this.enemies.forEach(enemy => {
             this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height)
