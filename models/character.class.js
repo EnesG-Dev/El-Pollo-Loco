391d42ed93512c7d588981413a4c19bd484f5ce4
Enes Geldi <geldi.dev@gmail.com>
2024-12-15 23:41:20 +0100

    animation & move character left, right

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject {
     width = 100;
     y = 155
     world;
+    speed = 3;
 
     IMAGES_IDLE = [
         'img/2_character_pepe/1_idle/idle/I-1.png',
@@ -17,28 +18,59 @@ class Character extends MovableObject {
         'img/2_character_pepe/1_idle/idle/I-10.png',
     ];
 
+    IMAGES_WALK = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png',
+    ];
+
     constructor() {
         super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png')
         this.loadImages(this.IMAGES_IDLE);
-        this.animate(this.IMAGES_IDLE);
+        this.loadImages(this.IMAGES_WALK);
+        this.animate();
     }
 
     /**
      * 
      * @param {[string]} imgStack - Stack of image paths that are animated
      */
-    animate(imgStack) {
-        setInterval(() => {
+    animate() {
 
-            if (this.world.keyboard.RIGHT) {
-                   
-                let i = this.curentImage % imgStack.length; // test
-                let path = imgStack[i];
-                this.img = this.imageCache[path];
-                // this.curentImage == imgStack.length -1 ? this.curentImage = 0 : this.curentImage++;
-                this.curentImage++; // test
-            }
+        // IDLE
+        setInterval(() => {
+            let i = this.curentImage % this.IMAGES_IDLE.length; // test
+            let path = this.IMAGES_IDLE[i];
+            this.img = this.imageCache[path];
+            // this.curentImage == this.IMAGES_IDLE.length -1 ? this.curentImage = 0 : this.curentImage++;
+            this.curentImage++; // test
         }, 500);
+
+        // WALK
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+            
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 100 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+
+                let i = this.curentImage % this.IMAGES_WALK.length;
+                let path = this.IMAGES_WALK[i];
+                this.img = this.imageCache[path];
+                this.curentImage++;
+            }
+        }, 50);
+
+
     }
 
 
